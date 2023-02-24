e0a06be315ecbb781978be6a21ca5d6427ae1a8f
YUVARAJ05 <126266379+YUVARAJ05@users.noreply.github.com>
2023-02-24 20:50:12 +0530

    Add files via upload

---
+++ DAY-1.py
@@ -0,0 +1,9 @@
+
+
+a=int(input("enter the number1:"))
+b= int(input("enter the number2:"))
+print("the sum of two numbers is ",a+b)
+print("the multiply of two number is",a*b)
+print("the subtract of two number is", a-b)
+print("the division of two number is",a/b)
+
